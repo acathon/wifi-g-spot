bd45f26aa4da4f5c9c3f355d7580ef277e0f74d4
Mustapha Essouri <Acathon@users.noreply.github.com>
2017-08-11 10:11:28 +0100

    Ajoutez des fichiers projet.

---
+++ GSpot/API.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSpot
+{
+    class API
+    {
+        public static bool IsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal p = new WindowsPrincipal(id);
+            return p.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public void RestartElevated()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = true;
+            startInfo.CreateNoWindow = true;
+            startInfo.WorkingDirectory = Environment.CurrentDirectory;
+            startInfo.FileName = System.Windows.Forms.Application.ExecutablePath;
+            startInfo.Verb = "runas";
+            try
+            {
+                Process p = Process.Start(startInfo);
+            }
+            catch
+            {
+                EventLog.GetEventLogs();
+            }
+
+            System.Windows.Forms.Application.Exit();
+        }
+
+
+        private void Hotspot(string ssid, string key, bool status)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe");
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.UseShellExecute = false;
+            Process process = Process.Start(processStartInfo);
+
+            if(process != null)
+            {
+                if (status)
+                {
+                    process.StandardInput.WriteLine("netsh wlan set hostednetwork mode=allow ssid=" + ssid + " key=" + key);
+                    process.StandardInput.WriteLine("netsh wlan start hostednetwork");
+                    process.StandardInput.Close();
+                }
+                else
+                {
+                    process.StandardInput.WriteLine("netsh wlan stop hostednetwork");
+                    process.StandardInput.Close();
+                }
+            }
+        }
+
+    }
+}
